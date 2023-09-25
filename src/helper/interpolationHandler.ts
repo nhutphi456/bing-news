547f4c9466f27c41cd66106e6f742fe96834b700
nhutphi456 <vonhutphi>
2023-09-25 14:24:49 +0700

    remove eval from ngIfHandler

--- src/helper/interpolationHandler.ts
+++ src/helper/interpolationHandler.ts
@@ -2,23 +2,43 @@ import { Component } from "../interfaces/component";
 import { ViewHandler } from "../interfaces/viewHandler";
 
 export class InterpolationHandler extends ViewHandler {
-
   public handle(instance: InstanceType<Component>, view: string): string {
-    // view = view.replace(/{{(.*?)}}/g, (match, key) => {
-    //   const keys = key.split(".");
-    //   let value = instance;
-
-    //   for (const k of keys) {
-    //     value = value[k];
-    //     if (value === undefined) return match; // Return the placeholder if the value is undefined
-    //   }
-    //   return value;
-    // });
-
     view = view.replace(/{{(.*?)}}/g, (match, key) => {
-      return eval(`instance.${key}`);
+      const ternaryExpression = this.extractTernary(key);
+
+      if (ternaryExpression) {
+        const { condition, expression1, expression2 } = ternaryExpression;
+        const prop = condition.split("===");
+        const evalExpression = new Function(`return this.${prop[0]} === ${prop[1]}`);
+        const value = evalExpression.call(instance);
+
+        return value ? this.replaceSingleQuote(expression1) : this.replaceSingleQuote(expression2);
+      } else {
+        return this.getNestedPropertyValue(instance, key);
+      }
     });
 
     return super.handle(instance, view);
+  }
+
+  private extractTernary(
+    expression: string
+  ): { condition: string; expression1: string; expression2: string } | null {
+    const ternaryExpressionPattern = /^(.*?) \? (.*?) : (.*)$/;
+    const match = expression.match(ternaryExpressionPattern);
+
+    if (match && match.length === 4) {
+      return {
+        condition: match[1].trim(),
+        expression1: match[2].trim(),
+        expression2: match[3].trim(),
+      };
+    } else {
+      return null;
+    }
+  }
+
+  private replaceSingleQuote(str: string): string {
+    return str.replace(/'/g, "");
   }
 }
